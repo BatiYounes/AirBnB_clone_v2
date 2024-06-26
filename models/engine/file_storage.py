18a4ef820970dcd0df0ea87ca0ec1efea33a92fa
Batiyounes <Younesbati007@gmail.com>
2024-06-26 22:14:43 +0000

    doing the task of 0x02 Airbnb Clone V2 mYSQL

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,73 +1,59 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-import shlex
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format
-    Attributes:
-        __file_path: JSON file path
-        __objects: objects dictionary
-    """
+    """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
-        _dict = {}
+        """Returns a dictionary of models currently in storage, filtered by class if provided"""
         if cls:
-            _dict_ = self.__objects
-            for key in _dict_:
-                _dict_partition = key.replace('.', ' ')
-                _dict_partition = shlex.split(_dict_partition)
-                if (_dict_partition[0] == cls.__name__):
-                    _dict[key] = self.__objects[key]
-            return _dict
-        else:
-            return self.__objects
+            return {key: value for key, value in FileStorage.__objects.items() if isinstance(value, cls)}
+        return FileStorage.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary
-        Args:
-            obj: given object
-        """
-        if obj:
-            key = f'{type(obj).__name__}.{obj.id}'
-            self.__objects[key] = obj
+        """Adds new object to storage dictionary"""
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
-        _dict = {}
-        for key, value in self.__objects.items():
-            _dict[key] = value.to_dict()
-        with open(self.__file_path, 'w', encoding="UTF-8") as f:
-            json.dump(_dict, f)
+        with open(FileStorage.__file_path, 'w') as f:
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
         try:
-            with open(self.__file_path, 'r', encoding="UTF-8") as f:
-                for key, value in (json.load(f)).items():
-                    value = eval(value["__class__"])(**value)
-                    self.__objects[key] = value
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """delete object from a list of objects."""
+        """Deletes obj from __objects if it's inside"""
         if obj:
-            obj_to_delete = f'{type(obj).__name__}.{obj.id}'
-            del self.__objects[obj_to_delete]
-
-    def close(self):
-        """calls reload method for deserializing the JSON file
-        to objects"""
-        self.reload()
+            obj_key = obj.to_dict()['__class__'] + '.' + obj.id
+            if obj_key in FileStorage.__objects:
+                del FileStorage.__objects[obj_key]
