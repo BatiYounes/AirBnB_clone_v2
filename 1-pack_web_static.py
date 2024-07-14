58d6669e0c4433c1c307b7dc48b77ebcd388c203
Younesbati <younesbati007@gmail.come>
2024-07-14 12:19:05 -0700

    doing the task of 0x02 Airbnb Clone V2 mYSQL

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-# Fabric script to generate a .tgz archive from the contents of the web_static folder
+# Fabric script to generate a .tgz archive from the web_static folder
 
 from fabric.api import local
 from datetime import datetime
 import os
 
+
 def do_pack():
     """
-    Generates a .tgz archive from the contents of the web_static folder
+    Generates a .tgz archive from the contents of the web_static folder.
     Returns:
-        The archive path if the archive has been correctly generated.
-        Otherwise, it should return None.
+        str: The archive path if the archive has been correctly generated,
+             otherwise None.
     """
     try:
         if not os.path.exists("versions"):
