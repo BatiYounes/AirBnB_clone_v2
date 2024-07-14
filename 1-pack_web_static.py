210517d1bdd3204b6651472c13b420dcec7fd90a
Younesbati <younesbati007@gmail.come>
2024-07-14 12:16:03 -0700

    doing the task of 0x02 Airbnb Clone V2 mYSQL

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,13 +6,18 @@ from datetime import datetime
 import os
 
 def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder"""
+    """
+    Generates a .tgz archive from the contents of the web_static folder
+    Returns:
+        The archive path if the archive has been correctly generated.
+        Otherwise, it should return None.
+    """
     try:
         if not os.path.exists("versions"):
-            os.mkdir("versions")
+            os.makedirs("versions")
         current_time = datetime.now().strftime("%Y%m%d%H%M%S")
         archive_path = "versions/web_static_{}.tgz".format(current_time)
         local("tar -cvzf {} web_static".format(archive_path))
         return archive_path
-    except Exception as e:
+    except Exception:
         return None
